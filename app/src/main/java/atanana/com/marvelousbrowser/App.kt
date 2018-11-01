091ba8b887f5d9c017a9d6a0fdfd2a45de51750c
Andrei <tanana@email.ru>
2018-11-01 22:58:38 +0300

    create basic retrofit

--- app/src/main/java/atanana/com/marvelousbrowser/App.kt
+++ app/src/main/java/atanana/com/marvelousbrowser/App.kt
@@ -1,7 +1,11 @@
 package atanana.com.marvelousbrowser
 
 import android.app.Application
+import org.koin.android.ext.android.startKoin
 
 class App : Application() {
-
+    override fun onCreate() {
+        super.onCreate()
+        startKoin(this, listOf(mainModule))
+    }
 }
---
+++ app/src/main/java/atanana/com/marvelousbrowser/modules.kt
@@ -0,0 +1,28 @@
+package atanana.com.marvelousbrowser
+
+import okhttp3.OkHttpClient
+import org.koin.dsl.module.module
+import retrofit2.Retrofit
+import retrofit2.converter.moshi.MoshiConverterFactory
+import java.util.concurrent.TimeUnit
+
+val mainModule = module {
+    single { buildRetrofit() }
+}
+
+private fun buildRetrofit(): Retrofit {
+    return Retrofit.Builder()
+        .baseUrl("https://gateway.marvel.com:443/v1/public")
+        .client(buildOkHttpClient())
+        .addConverterFactory(MoshiConverterFactory.create())
+        .build()
+}
+
+private const val TIMEOUT = 30L
+
+private fun buildOkHttpClient(): OkHttpClient =
+    OkHttpClient.Builder()
+        .connectTimeout(TIMEOUT, TimeUnit.SECONDS)
+        .readTimeout(TIMEOUT, TimeUnit.SECONDS)
+        .writeTimeout(TIMEOUT, TimeUnit.SECONDS)
+        .build()
