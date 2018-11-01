d9e75a8ed30dcf30a2d655d51b60f56d25f8ee28
Andrei <tanana@email.ru>
2018-11-01 23:24:24 +0300

    add retrofit coroutine adapter

--- app/src/main/java/atanana/com/marvelousbrowser/modules.kt
+++ app/src/main/java/atanana/com/marvelousbrowser/modules.kt
@@ -1,6 +1,7 @@
 package atanana.com.marvelousbrowser
 
 import atanana.com.marvelousbrowser.web.MarvelAuthInterceptor
+import com.jakewharton.retrofit2.adapter.kotlin.coroutines.CoroutineCallAdapterFactory
 import okhttp3.OkHttpClient
 import org.koin.dsl.module.module
 import retrofit2.Retrofit
@@ -16,6 +17,7 @@ private fun buildRetrofit(): Retrofit {
         .baseUrl("https://gateway.marvel.com:443/v1/public")
         .client(buildOkHttpClient())
         .addConverterFactory(MoshiConverterFactory.create())
+        .addCallAdapterFactory(CoroutineCallAdapterFactory())
         .build()
 }
 
