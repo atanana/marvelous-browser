6924babe7e08e6dc93652a1e094326be0605ae8d
Andrei Tanana <andrei.tanana@deltatre.it>
2018-11-16 15:33:45 +0300

    remove live data

--- app/src/main/java/atanana/com/marvelousbrowser/screens/characters/CharactersPresenter.kt
+++ app/src/main/java/atanana/com/marvelousbrowser/screens/characters/CharactersPresenter.kt
@@ -1,31 +1,30 @@
 package atanana.com.marvelousbrowser.screens.characters
 
-import android.arch.lifecycle.Observer
-import android.arch.paging.LivePagedListBuilder
 import android.arch.paging.PagedList
 import atanana.com.marvelousbrowser.data.web.CharacterResponse
+import kotlinx.coroutines.Dispatchers
+import kotlinx.coroutines.GlobalScope
+import kotlinx.coroutines.launch
+import java.util.concurrent.Executors
 
-class CharactersPresenter(private val dataSourceFactory: CharactersDataSourceFactory) {
+class CharactersPresenter(private val charactersDataSource: CharactersDataSource) {
     private val pageConfig = PagedList.Config.Builder()
         .setPageSize(20)
         .setEnablePlaceholders(true)
         .build()
 
-    private val charactersData = LivePagedListBuilder<Int, CharacterResponse>(dataSourceFactory, pageConfig).build()
+    private val pagedList by lazy {
+        PagedList.Builder<Int, CharacterResponse>(charactersDataSource, pageConfig)
+            .setNotifyExecutor {
+                GlobalScope.launch(Dispatchers.Main) { it.run() }
+            }
+            .setFetchExecutor(Executors.newFixedThreadPool(2))
+            .build()
+    }
 
     var charactersView: CharactersView? = null
         set(value) {
             field = value
-            if (value != null) {
-                observeCharactersData(value)
-            }
+            value?.setCharacters(pagedList)
         }
-
-    private fun observeCharactersData(value: CharactersView) {
-        charactersData.observe(value, Observer { list ->
-            if (list != null) {
-                value.setCharacters(list)
-            }
-        })
-    }
 }
